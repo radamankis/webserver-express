6297d83ac4084d04d592f15181ab0ae551161b32
Ricardo <ricardosanchez0992@gmail.com>
2019-04-09 20:15:14 -0400

    Primer commit

---
+++ app-old.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+
+        res.writeHead(200, { 'Contrent-Type': 'application/json' });
+
+        let salida = {
+            nombre: 'Ricardo',
+            edad: 27,
+            url: req.url
+
+        }
+
+        res.write(JSON.stringify(salida));
+        //res.write('Hola mundo');
+        res.end();
+    })
+    .listen(8080);
+
+
+console.log("Escuchando el puerto 8080");
